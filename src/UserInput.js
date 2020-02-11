dd452c560d18e0effb745f1c76a48a62988053ed
Micah Lund <micah.r.lund@gmail.com>
2020-02-11 12:58:34 -0700

    Started working on a handleClick event for when data is submitted into the user inputs.

--- src/UserInput.js
+++ src/UserInput.js
@@ -13,6 +13,37 @@ constructor(props){
   }
  }
 
+ handleClick(event){
+    var apiBaseUrl = "http://localhost:4000/api/";
+    var self = this;
+    var payload={
+    "eid":this.state.eid,
+    "policyNum":this.state.policyNum
+    }
+    axios.post(apiBaseUrl+'login', payload)
+    .then(function (response) {
+    console.log(response);
+
+    if(response.data.code == 200){
+    console.log("Display new stats");
+    var uploadScreen=[];
+    uploadScreen.push(<UploadScreen appContext={self.props.appContext}/>)
+    self.props.appContext.setState({loginPage:[],uploadScreen:uploadScreen})
+    }
+    else if(response.data.code == 204){
+    console.log("Username password do not match");
+    alert("username password do not match")
+    }
+    else{
+    console.log("Username does not exists");
+    alert("Username does not exist");
+    }
+    })
+    .catch(function (error) {
+    console.log(error);
+    });
+    }
+
 render() {
     return (
       <div>
