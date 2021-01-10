88c6aee3b53d49ff6ecb14609e1b566ab5d82d79
Micah Lund <micah.r.lund@gmail.com>
2021-01-09 18:38:24 -0700

    Converted the UserInput component into a functional component.

--- src/UserInput.js
+++ src/UserInput.js
@@ -1,45 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Button from '@material-ui/core/Button';
-import Input from '@material-ui/core/Input';
-import { createMuiTheme } from '@material-ui/core/styles';
+// import Input from '@material-ui/core/Input';
+// import { createMuiTheme } from '@material-ui/core/styles';
 import './App.css';
 
 
-const theme = createMuiTheme({
-    palette: {
-        background: {
-        default: "#303030"
-        },
-        primary: {
-            main: '#ff4400',
-        },
-        secondary: {
-            light:'#0066ff',
-            main: '#0044ff',
-            contrastText: '#ffcc00'
-        } 
-    }
-});
+// const theme = createMuiTheme({
+//     palette: {
+//         background: {
+//         default: "#303030"
+//         },
+//         primary: {
+//             main: '#ff4400',
+//         },
+//         secondary: {
+//             light:'#0066ff',
+//             main: '#0044ff',
+//             contrastText: '#ffcc00'
+//         } 
+//     }
+// });
 
-class UserInput extends Component {
+const UserInput = () => {
 
-constructor(props){
-  super(props);
-  this.state={
-  eid:'',
-  policyNum:'',
-  stat: ''
-  }
- }
+// constructor(props){
+//   super(props);
+//   this.state={
+//   eid:'',
+//   policyNum:'',
+//   stat: ''
+//   }
+//  }
 
-render() {
     return (
       <div>
-        <MuiThemeProvider theme={theme}>
+        <MuiThemeProvider>
           <div>
            <h2>Associate Name</h2>
-             <Input
+             {/* <Input
                hintText="Enter a Policy Number"
                floatingLabelText="Policy Number 1 "
                onChange = {(event,newValue) => this.setState({policyNum:newValue})}
@@ -64,15 +63,16 @@ render() {
                size="small"
                floatingLabelText="Notes"
                onChange = {(event,newValue) => this.setState({policyNum:newValue})}
-               />
+               /> */}
              <br/>
-             <Button label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}>Submit</Button>
+             <Button label="Submit" primary={true} style={style}>Submit</Button>
+             {/* onClick={(event) => this.handleClick(event)} */}
          </div>
          </MuiThemeProvider>
       </div>
     );
   }
-}
+
 
 const style = {
  margin: 15,
